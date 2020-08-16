ac1bdfab5a515c872b185db89742d22eaf9eb08d
shiva prasad <shiva@secondsdigital.com>
2020-08-16 16:32:49 +0530

    user profile has been changes

--- application/views/profile.php
+++ application/views/profile.php
@@ -252,24 +252,23 @@ input[readonly] {
 															   <div class="col-md-6 profile-info two"> 
 																</div>
 																<!--/map-->
-															<div class="col-md-6 profile-info two">
+															<!-- <div class="col-md-6 profile-info two">
 															   <h3 class="inner-tittle two">Head Office </h3>
 																<div class="main-grid3 map">
- 	                                                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.08652150032!2d77.59919581443324!3d12.966315190859378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14677a117163%3A0xcc934a3cb6703eed!2sFull%20Basket%20Property%20Services%20Pvt%20Ltd%20%7C%20Leading%20Real%20Estate%20Company%20in%20Bangalore!5e0!3m2!1sen!2sin!4v1575961381619!5m2!1sen!2sin" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe> 
+ 	                                                      <iframe src="https://www.google.com/maps/embed" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe> 
 															<div class="gmap-info">
 																<h4> <i class="fa fa-map-marker"></i> <b><a href="#" class="text-dark">Prop Solutionss</a></b></h4>
-																<p>hayes Road, Richmond Circle</p>
+																<p>address</p>
                                                                 
                                                                 <p>Bangaluru Karnataka - 560025</p>
-																<p><!--Rera : PRM/KA/RERA/1251/446/AG/170926/000120<br>-->
-L:  080-2331375</p>
+																<p>Rera : PRM/KA/RERA/1<br>
+L:  080-</p>
 																</div>
 																	
 																	</div>
 																	
-																	
-																	<!--//map-->
-																</div>
+																	 
+																</div> -->
 																<div class="clearfix"></div>
 															</div>
 															
